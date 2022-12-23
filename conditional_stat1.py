c3015ea582af262d06ba522aa4a39e176d037669
Jeevitha-MC <mcjeevi1997@gmail.com>
2022-12-23 15:50:06 +0530

    Added conditional statement files

---
+++ conditional_stat1.py
@@ -0,0 +1,8 @@
+name1="Harry Potter"
+name=len(name1)
+if name < 8:
+    print('name must be atleast 3 characters')
+elif name > 50:
+    print('name cannot be a max of 50 characters')
+else:
+    print('name looks good')
